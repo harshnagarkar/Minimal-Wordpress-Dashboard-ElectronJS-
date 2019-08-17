ed8b4a2edaf66ff370b63610e86ab22d64893146
Harsh K. Nagarkar <hknagark@go.olemiss.edu>
2019-08-16 23:33:12 -0500

    local storage added to app

--- main.js
+++ main.js
@@ -27,12 +27,3 @@ app.on('activate', () => {
         createWindow()
     }
 })
-
-
-// const { BrowserWindow } = require('electron')
-
-// let win = new BrowserWindow({ width: 800, height: 1500 })
-// win.loadURL('http://github.com')
-
-// let contents = win.webContents
-// console.log(contents)
